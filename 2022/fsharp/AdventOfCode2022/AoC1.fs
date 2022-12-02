f0081c6c344a4d5cf19ed60179889420e212893a
David Garcinuño Enríquez <d.garcinuno@peza.io>
2022-12-02 07:11:24 +0100

    Completed Aoc1 2022 F#

--- 2022/fsharp/AdventOfCode2022/AoC1.fs
+++ 2022/fsharp/AdventOfCode2022/AoC1.fs
@@ -7,4 +7,38 @@ let aoc1 () =
 
     let inputLines = readInputFile "AoC1.txt" |> Array.toList
 
-    ()
+    let mapElement el =
+        match el with
+        | "" -> None
+        | _ -> Some (el |> int)
+
+    let input = inputLines |> List.map mapElement
+
+    let reduceInput lists element =
+        match element with
+        | None -> [] :: lists
+        | Some number ->
+            match lists with
+            | [] -> [[ number ]]
+            | first :: remaining -> (number :: first) :: remaining
+
+    let numbers = 
+        input
+        |> List.fold reduceInput []
+        |> List.map List.sum
+
+    let result1 =
+        numbers
+        |> List.max
+        |> string
+
+    printFirstStarResult result1
+
+    let result2 = 
+        numbers
+        |> List.sortDescending
+        |> List.take 3
+        |> List.sum
+        |> string
+    
+    printFirstStarResult result2
