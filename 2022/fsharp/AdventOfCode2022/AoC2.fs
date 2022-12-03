af60c26fdfb324c8ee7aed9179ff7392dbd62e9d
David Garcinuño Enríquez <d.garcinuno@peza.io>
2022-12-03 18:35:42 +0100

    Completed Aoc2 2022 Part 1 F#

--- 2022/fsharp/AdventOfCode2022/AoC2.fs
+++ 2022/fsharp/AdventOfCode2022/AoC2.fs
@@ -2,9 +2,73 @@
 
 open Utils
 
+type Turn = 
+| Rock
+| Paper
+| Scissors
+
+type GameResult = 
+| Win
+| Loss
+| Draw
+
 let aoc2 () =
     printBanner 2
 
     let inputLines = readInputFile "AoC2.txt" |> Array.toList
 
-    ()
+    let mapOpponentTurn x = 
+        match x with
+        | "A" -> Rock
+        | "B" -> Paper
+        | "C" -> Scissors
+        | _ -> failwith "Invalid input for opponent turn"
+
+    let mapOwnTurn x = 
+        match x with
+        | "X" -> Rock
+        | "Y" -> Paper
+        | "Z" -> Scissors
+        | _ -> failwith "Invalid input for own turn"
+
+    let shapeScore x =
+        match x with
+        | Rock -> 1
+        | Paper -> 2
+        | Scissors -> 3
+    
+    let resultScore x = 
+        match x with
+        | Win -> 6
+        | Loss -> 0
+        | Draw -> 3
+
+    let game mine opponent =
+        match mine with
+        | Rock ->
+            match opponent with
+            | Rock -> Draw
+            | Paper -> Loss
+            | Scissors -> Win
+        | Paper ->
+            match opponent with
+            | Rock -> Win
+            | Paper -> Draw
+            | Scissors -> Loss
+        | Scissors ->
+            match opponent with
+            | Rock -> Loss
+            | Paper -> Win
+            | Scissors -> Draw
+
+    let result1 =
+        inputLines
+        |> List.map (split ' ')
+        |> List.map (fun [| x; y |] -> (x |> mapOpponentTurn, y |> mapOwnTurn))
+        |> List.map (fun (opponent, mine) -> (shapeScore mine) + (resultScore (game mine opponent)))
+        |> List.sum
+        |> string
+
+    printFirstStarResult result1
+
+    
